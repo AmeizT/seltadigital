5d86086bd221ac0e0d39ce99264139762f8f1339
Your Name <37455928+AmeizT@users.noreply.github.com>
2022-09-13 11:10:37 +0200

    added web manifest

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const myPWA = require("next-pwa");
 
-module.exports = nextConfig
+module.exports = myPWA({
+  reactStrictMode: true,
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  }
+})
